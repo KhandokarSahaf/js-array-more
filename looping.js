eca0d1c4334b2d71f97dd0d8f955ab051c3e8e24
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-07-24 20:39:17 +0600

    first commit

---
+++ looping.js
@@ -0,0 +1,25 @@
+const friends = ["Asif", "Samir", "Sadman", "Sahaf"];
+let z = 0;
+while (z < friends.length) {
+//   console.log(friends[z]);
+  z++;
+}
+
+for (const friend of friends) {
+  //   console.log(friend);
+}
+for (i = 0; i < friends.length; i++) {
+  //   console.log(friends[i]);
+}
+
+// /  *//  */
+const number = [11, 22, 33, 44, 55, 66, 77, 88, 99];
+
+for (let b = 0; b < number.length; b++) {
+  //   console.log(number[b]);
+}
+let b = 0;
+while (b < number.length) {
+//   console.log(number[b]);
+  b++;
+}
