928dbb8140604af239cf6dba2c0bff0bdc91ea02
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-07-30 09:57:52 +0600

    push looping1

---
+++ looping1.js
@@ -0,0 +1,10 @@
+const fds = ["Sadman", "Sahaf", "Nafees", "Nehal"];
+
+for (const fd of fds) {
+  // console.log(fd);
+}
+
+for (i = 0; i < fds.length; i++) {
+  const fdes = fds[i];
+  console.log(fdes);
+}
